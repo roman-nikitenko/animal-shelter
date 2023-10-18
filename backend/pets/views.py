da634c3db3e547528bfd36025efeac0871693c11
Amirammi <amirammina@gmail.com>
2023-10-18 15:17:38 +0300

    Created PetViewSet with get_serializer_class in views.py

--- backend/pets/views.py
+++ backend/pets/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import Pet
+from .serializers import PetSerializer, PetListSerializer, PetDetailSerializer
+
+
+class PetViewSet(viewsets.ModelViewSet):
+    queryset = Pet.objects.select_related("animal_type")
+    serializer_class = PetSerializer
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return PetListSerializer
+        if self.action == "retrieve":
+            return PetDetailSerializer
+        return self.serializer_class
