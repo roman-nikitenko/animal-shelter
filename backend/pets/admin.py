5fc54bc040641741fb1730a93b3340f6618dfaee
Amirammi <amirammina@gmail.com>
2023-10-18 15:12:55 +0300

    Registered models in admin.py

--- backend/pets/admin.py
+++ backend/pets/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
 
-# Register your models here.
+from .models import Pet, PetType
+
+
+@admin.register(Pet)
+class PetAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "animal_type", "gender", "age")
+    list_filter = ("animal_type", )
+    search_fields = ("id", "name")
+
+
+admin.site.register(PetType)
+admin.site.unregister(Group)
