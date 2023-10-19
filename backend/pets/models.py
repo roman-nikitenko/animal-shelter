33f7f28e8aaa933ff05a7cb003952b5e0d0d7905
Amirammi <amirammina@gmail.com>
2023-10-19 15:22:12 +0300

    Added filters by animal_type, color, gender & size

--- backend/pets/models.py
+++ backend/pets/models.py
@@ -33,7 +33,7 @@ class Pet(models.Model):
 
     name = models.CharField(max_length=63)
     animal_type = models.ForeignKey(PetType, on_delete=models.CASCADE)
-    gender = models. CharField(max_length=6, choices=GENDER_CHOICES)
+    gender = models.CharField(max_length=6, choices=GENDER_CHOICES)
     age = models.IntegerField()
     breed = models.CharField(max_length=63, default="mongrel")
     size = models.CharField(max_length=6, choices=SIZE_CHOICES)
--- backend/pets/views.py
+++ backend/pets/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from drf_spectacular.types import OpenApiTypes
+from drf_spectacular.utils import extend_schema, OpenApiParameter
 
 from .models import Pet
 from .serializers import PetSerializer, PetListSerializer, PetDetailSerializer
@@ -14,3 +16,57 @@ class PetViewSet(viewsets.ModelViewSet):
         if self.action == "retrieve":
             return PetDetailSerializer
         return self.serializer_class
+
+    def get_queryset(self):
+        animal_type = self.request.query_params.get("animal_type")
+        color = self.request.query_params.get("color")
+        gender = self.request.query_params.get("gender")
+        size = self.request.query_params.get("size")
+
+        if animal_type:
+            self.queryset = self.queryset.filter(
+                animal_type__name__icontains=animal_type
+            )
+
+        if color:
+            self.queryset = self.queryset.filter(color__icontains=color)
+
+        if gender:
+            self.queryset = self.queryset.filter(gender=gender)
+
+        if size:
+            self.queryset = self.queryset.filter(size__icontains=size)
+
+        return self.queryset
+
+    @extend_schema(
+        # extra parameters added to the schema
+        parameters=[
+            OpenApiParameter(
+                name="animal_type",
+                type=OpenApiTypes.STR,
+                description="Filter pets by "
+                            "pet types (ex. ?animal_type=Dog)"
+            ),
+            OpenApiParameter(
+                name="color",
+                type=OpenApiTypes.STR,
+                description="Filter pets by "
+                            "color (ex. ?color=black)"
+            ),
+            OpenApiParameter(
+                name="gender",
+                type=OpenApiTypes.STR,
+                description="Filter pets by "
+                            "gender (ex. ?gender=Male or ?gender=Female)."
+            ),
+            OpenApiParameter(
+                name="size",
+                type=OpenApiTypes.STR,
+                description="Filter pets by "
+                            "size (ex. ?size=MIDDLE)"
+            )
+        ]
+    )
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
