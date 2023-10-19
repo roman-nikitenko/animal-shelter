e094151934c3f49d2be62e529d4b6ca1c9672749
Roman Nikitenko <Lomkaaa@ukr.net>
2023-10-19 22:46:05 +0300

    Edited readme file, added in readme file link to work demo site.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import './App.css';
+import { HomePage } from './components/HomePage';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <HomePage />
   );
 }
 
