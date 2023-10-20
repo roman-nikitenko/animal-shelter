ff374863437a1d4fc75218d244cc3c430d9fa2df
Amirammi <amirammina@gmail.com>
2023-10-20 15:13:29 +0300

    Fixed bug with api/users/ endpoint in project urls.py

--- backend/animal_shelter/urls.py
+++ backend/animal_shelter/urls.py
@@ -27,7 +27,7 @@ from drf_spectacular.views import (
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/pets/", include("pets.urls", namespace="pets")),
-    path("api/users/", include("user.urls"), namespace="user"),
+    path("api/users/", include("user.urls", namespace="user")),
     path(
         "api/appointments/",
         include("appointment.urls", namespace="appointment")
