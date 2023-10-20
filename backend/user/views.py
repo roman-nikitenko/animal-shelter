d794de0b9216612c60940c01b1c81d97567dd949
Amirammi <amirammina@gmail.com>
2023-10-20 12:58:15 +0300

    Added views for User

--- backend/user/views.py
+++ backend/user/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
-# Create your views here.
+from .serializers import UserSerializer, CreateUserSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    """Users can register"""
+    serializer_class = CreateUserSerializer
+
+
+class ManageUserView(generics.RetrieveUpdateDestroyAPIView):
+    """Users can manage their accounts.
+    Change their information and delete the account."""
+    serializer_class = UserSerializer
+    authentication_classes = (JWTAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self):
+        return self.request.user
