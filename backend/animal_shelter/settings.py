278b8871fb5f9bf9a5abad059ab59fc4c719cc88
Amirammi <amirammina@gmail.com>
2023-10-20 20:44:43 +0300

    Added "happy-paws-pqwx.onrender.com" to ALLOWED_HOSTS

--- backend/animal_shelter/settings.py
+++ backend/animal_shelter/settings.py
@@ -31,7 +31,9 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "happy-paws-pqwx.onrender.com", "localhost", "127.0.0.1"
+]
 
 
 # Application definition
