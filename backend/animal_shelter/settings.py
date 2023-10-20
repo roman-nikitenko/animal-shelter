04025281c28f479d0eb584d4137241b5e74c4a77
Amirammi <amirammina@gmail.com>
2023-10-20 18:58:16 +0300

    Fixed requirements.txt & settings.py

--- backend/animal_shelter/settings.py
+++ backend/animal_shelter/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -133,6 +134,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = (BASE_DIR / "static",)
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+ASSETS_ROOT = "/static/assets"
 
 MEDIA_URL = "/media/"
 
