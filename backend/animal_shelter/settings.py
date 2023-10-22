4b270bbce2dbdff2fc2f617f6a1ee4b4a1157bd3
MrYuriy <yuriybabiyk@gmail.com>
2023-10-22 13:42:51 +0200

    Add logout functionality

--- backend/animal_shelter/settings.py
+++ backend/animal_shelter/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "rest_framework_simplejwt",
+    "rest_framework_simplejwt.token_blacklist",
     "drf_spectacular",
     "phonenumber_field",
     "pets",
