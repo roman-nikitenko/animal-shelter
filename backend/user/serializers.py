683cd22899b2e2d381167ea321b311448ca3ceca
Amirammi <amirammina@gmail.com>
2023-10-23 14:38:10 +0300

    Fixed password settings in serializers

--- backend/user/serializers.py
+++ backend/user/serializers.py
@@ -25,7 +25,7 @@ class CreateUserSerializer(serializers.ModelSerializer):
             "password",
             "phone_number"
         )
-        extra_kwargs = {"password": {"write_only": True, "min_length": 5}}
+        extra_kwargs = {"password": {"write_only": True}}
 
     def create(self, validated_data):
         """Create a new user with encrypted password and return it"""
@@ -56,7 +56,7 @@ class UserSerializer(serializers.ModelSerializer):
             "phone_number",
             "is_staff")
         read_only_fields = ("is_staff",)
-        extra_kwargs = {"password": {"write_only": True, "min_length": 5}}
+        extra_kwargs = {"password": {"write_only": True}}
 
     def update(self, instance, validated_data):
         """Update a user, set the password correctly and return it"""
