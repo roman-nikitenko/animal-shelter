c97d0fcaa0f6d2f7f23ad54de29cf15f3b558f70
MrYuriy <yuriybabiyk@gmail.com>
2023-10-24 06:36:13 +0200

    Fix bug with updating picture users profile

--- backend/user/serializers.py
+++ backend/user/serializers.py
@@ -55,6 +55,7 @@ class UserSerializer(serializers.ModelSerializer):
             "first_name",
             "last_name",
             "phone_number",
+            "profile_picture",
             "is_staff")
         read_only_fields = ("is_staff",)
         extra_kwargs = {"password": {"write_only": True, "min_length": 5}}
