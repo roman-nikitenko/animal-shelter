4ac3237db9cf47900dbf24b650792eb3f4f6303e
Amirammi <amirammina@gmail.com>
2023-10-25 19:11:17 +0300

    Changed name "register/" endpoint to "create-user"

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -8,7 +8,7 @@ from rest_framework_simplejwt.views import (
 from .views import CreateUserView, ManageUserView, UserLogoutAPIView
 
 urlpatterns = [
-    path("register/", CreateUserView.as_view(), name="create"),
+    path("register/", CreateUserView.as_view(), name="create-user"),
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
