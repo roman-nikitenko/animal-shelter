944efb36b003e30933712e18914d0062b68b3b74
Amirammi <amirammina@gmail.com>
2023-10-25 19:10:39 +0300

    Added "id" field to CreateUserSerializer

--- backend/user/serializers.py
+++ backend/user/serializers.py
@@ -19,6 +19,7 @@ class CreateUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
         fields = (
+            "id",
             "email",
             "first_name",
             "last_name",
