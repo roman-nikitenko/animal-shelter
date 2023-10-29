284356ad4855e290e9e64ede5093752b788ffd76
MrYuriy <yuriybabiyk@gmail.com>
2023-10-29 19:40:28 +0100

    Add new techologi to sending message

--- backend/notification/utils.py
+++ backend/notification/utils.py
@@ -7,7 +7,7 @@ from telebot import TeleBot
 bot = TeleBot(settings.BOT_TOKEN)
 @bot.message_handler(func=lambda message: True)
 def handle_message(message_data):
-    print(message)
+    print(message_data)
     # Перевірте, чи правильно визначена змінна message і чи передається вона до цієї функції
     text = message_data['message']['text'].lower()  # Приводим текст к нижнему регистру для удобства сравнения
 
