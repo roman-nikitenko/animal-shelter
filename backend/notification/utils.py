7fda2d54b4d87f195960d9888015e043baa90f3a
MrYuriy <yuriybabiyk@gmail.com>
2023-10-29 19:34:55 +0100

    Try fix dict error

--- backend/notification/utils.py
+++ backend/notification/utils.py
@@ -9,11 +9,13 @@ bot = TeleBot(settings.BOT_TOKEN)
 def handle_message(message):
     print(message)
     # Перевірте, чи правильно визначена змінна message і чи передається вона до цієї функції
-    text = message.text.lower()  # Приводим текст к нижнему регистру для удобства сравнения
+    text = message_data['message']['text'].lower()  # Приводим текст к нижнему регистру для удобства сравнения
 
     if text == '/start':
-        bot.send_message(message.chat.id, "Ласкаво просимо! Я ваш бот. Чим я можу допомогти?")
+        bot.send_message(message_data['message']['chat']['id'], "Ласкаво просимо! Я ваш бот. Чим я можу допомогти?")
     elif text == '/help':
-        bot.send_message(message.chat.id, "Це бот для чогось. Ось доступні команди:\n/start - почати\n/help - отримати довідку")
+        bot.send_message(message_data['message']['chat']['id'],
+                         "Це бот для чогось. Ось доступні команди:\n/start - почати\n/help - отримати довідку")
     else:
-        bot.send_message(message.chat.id, "Я не розумію цю команду. Введіть /help для отримання довідки.")
+        bot.send_message(message_data['message']['chat']['id'],
+                         "Я не розумію цю команду. Введіть /help для отримання довідки.")
