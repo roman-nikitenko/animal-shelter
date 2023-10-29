ed0196f621bd18ec43910a3f58e3cee362684020
MrYuriy <yuriybabiyk@gmail.com>
2023-10-29 18:59:20 +0100

    Add new techologi ti sending message

--- backend/notification/views.py
+++ backend/notification/views.py
@@ -10,7 +10,6 @@ from .serializers import NotificationSerializer
 from secrets import token_urlsafe
 
 from django.http import JsonResponse
-from telegram.ext import CallbackContext
 from .utils import handle_telegram_update
 
 
