e606f984b5de25d1ae90e12b847d4fdda14ebe37
Amirammi <amirammina@gmail.com>
2023-10-29 21:21:28 +0200

    Add static url to project urls.py

--- backend/animal_shelter/urls.py
+++ backend/animal_shelter/urls.py
@@ -43,4 +43,8 @@ urlpatterns = [
         SpectacularRedocView.as_view(url_name="schema"),
         name="redoc"
     ),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(
+    settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+) + static(
+    settings.STATIC_URL, document_root=settings.STATIC_ROOT
+)
