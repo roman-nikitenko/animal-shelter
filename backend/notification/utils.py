eecba53fa1b122680b5471a3ac2ddfbc2ce976d1
MrYuriy <yuriybabiyk@gmail.com>
2023-10-29 20:52:16 +0100

    Add token veryfi entering

--- backend/notification/utils.py
+++ backend/notification/utils.py
@@ -1,21 +1,23 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from django.http import JsonResponse
+from .models import Notification
 from animal_shelter import settings
 from telebot import TeleBot
 
 bot = TeleBot(settings.BOT_TOKEN)
+
+
 @bot.message_handler(func=lambda message: True)
 def handle_message(message_data):
     print(message_data)
-    # Перевірте, чи правильно визначена змінна message і чи передається вона до цієї функції
+    chat_id = message_data['message']['chat']['id']
     text = message_data['message']['text'].lower()  # Приводим текст к нижнему регистру для удобства сравнения
 
     if text == '/start':
-        bot.send_message(message_data['message']['chat']['id'], "Ласкаво просимо! Я ваш бот. Чим я можу допомогти?")
-    elif text == '/help':
-        bot.send_message(message_data['message']['chat']['id'],
-                         "Це бот для чогось. Ось доступні команди:\n/start - почати\n/help - отримати довідку")
+        bot.send_message(chat_id, "Enter your veryfi token")
+    elif "token:" in text:
+        token = text.split("token:")[1]
+        user = Notification.objects.get(telegram_token=token).user
+        print(user)
+
     else:
         bot.send_message(message_data['message']['chat']['id'],
                          "Я не розумію цю команду. Введіть /help для отримання довідки.")
