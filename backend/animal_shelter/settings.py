da1c1e260c6ffa0b61c943252dc588932b971f71
Amirammi <amirammina@gmail.com>
2023-10-30 11:03:46 +0200

    Add new server to CORS_ALLOWED_ORIGINS

--- backend/animal_shelter/settings.py
+++ backend/animal_shelter/settings.py
@@ -181,5 +181,6 @@ SPECTACULAR_SETTINGS = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "http://localhost:3000"
+    "http://localhost:3000",
+    "https://roman-nikitenko.github.io/animal-shelter/",
 ]
